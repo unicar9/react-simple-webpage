1d24594168c1d7709b6c143e7afb002ae48dab9e
Weijia Li <unicar9@gmail.com>
2017-09-20 15:58:12 +1000

    link triggered and tested

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NewsList, BigCards, MidCards, SmallCards } from '../app/index.js';
 import renderer from 'react-test-renderer';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
 
 /* Invariant Violation: _registerComponent(...): Target container is not a DOM element.
 this error occurs when I export obove components inside the same file which is calling ReactDOM.render.
@@ -8,7 +10,7 @@ jest.mock('react-dom') is used here to avoid the conflict*/
 jest.mock('react-dom');
 
 
-describe('whole webpage renders correctly', () => {
+describe('this simple webpage', () => {
   it('BigCards renders correctly', () => {
     const component = renderer.create(<BigCards />);
     const json = component.toJSON();
@@ -32,4 +34,29 @@ describe('whole webpage renders correctly', () => {
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
+
+  it('should handle the click event for BigCards', () => {
+    window.alert = jest.fn();
+    const output = shallow(<BigCards />);
+
+    output.find('a').simulate('click');
+    expect(window.alert).toHaveBeenCalledWith('clicked!');
+  });
+
+  it('should handle the click event for MidCards', () => {
+    window.alert = jest.fn();
+    const output = shallow(<MidCards />);
+
+    output.find('a').simulate('click');
+    expect(window.alert).toHaveBeenCalledWith('clicked!');
+  });
+
+  it('should handle the click event for SmallCards', () => {
+    window.alert = jest.fn();
+    const output = shallow(<SmallCards />);
+
+    output.find('a').simulate('click');
+    expect(window.alert).toHaveBeenCalledWith('clicked!');
+  });
+
 });
--- app/index.js
+++ app/index.js
@@ -3,68 +3,78 @@ import ReactDOM from 'react-dom';
 
 class NewsList extends Component {
 
+
   render() {
     return (
       <div className="wrapper">
         <div className="big">
-          <BigCards image="1" title="No apologies James Taylor no right answer my scarf we are. Never getting back together candles Tree Paine all too."/>
-          <BigCards image="2" title="No apologies James Taylor no right answer my scarf we are. Never getting back together candles Tree Paine all too."/>
-          <BigCards image="3" title="No apologies James Taylor no right answer my scarf we are. Never getting back together candles Tree Paine all too."/>
+          <BigCards image="1" title="No apologies James Taylor no right answer my scarf we are. Never getting back together candles Tree Paine all too." url=""/>
+          <BigCards image="2" title="No apologies James Taylor no right answer my scarf we are. Never getting back together candles Tree Paine all too." url=""/>
+          <BigCards image="3" title="No apologies James Taylor no right answer my scarf we are. Never getting back together candles Tree Paine all too." url=""/>
         </div>
 
         <div className="mid">
-          <MidCards image="0" title="Sad all too well presumptuous no apologies no apologies break and."/>
-          <MidCards image="1" title="Sad all too well presumptuous no apologies no apologies break and."/>
-          <MidCards image="2" title="Sad all too well presumptuous no apologies no apologies break and."/>
-          <MidCards image="3" title="Sad all too well presumptuous no apologies no apologies break and."/>
-          <MidCards image="4" title="Sad all too well presumptuous no apologies no apologies break and."/>
-          <MidCards image="5" title="Sad all too well presumptuous no apologies no apologies break and."/>
+          <MidCards image="0" title="Sad all too well presumptuous no apologies no apologies break and." url=""/>
+          <MidCards image="1" title="Sad all too well presumptuous no apologies no apologies break and." url=""/>
+          <MidCards image="2" title="Sad all too well presumptuous no apologies no apologies break and." url=""/>
+          <MidCards image="3" title="Sad all too well presumptuous no apologies no apologies break and." url=""/>
+          <MidCards image="4" title="Sad all too well presumptuous no apologies no apologies break and." url=""/>
+          <MidCards image="5" title="Sad all too well presumptuous no apologies no apologies break and." url=""/>
 
-          <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-          <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-          <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+          <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit." url=""/>
+          <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit." url=""/>
+          <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit." url=""/>
 
           {/* writing hard-coded data is too redundant, so i choose to iterate an array and then map the number to MidCards and SmallCards component here */}
 
-          {[...Array(6)].map((x, i) => <MidCards image={i+5} title="Sad all too well presumptuous no apologies no apologies break and." key={i}/>)}
+          {[...Array(6)].map((x, i) => <MidCards image={i+5} title="Sad all too well presumptuous no apologies no apologies break and." url="#" key={i}/>)}
 
-          {[...Array(3)].map((x, i) => <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit." key={i}/>)}
+          {[...Array(3)].map((x, i) => <SmallCards title="Lorem ipsum dolor sit amet, consectetur adipisicing elit." url="#" key={i}/>)}
         </div>
       </div>
     );
   }
 }
 
-export default class BigCards extends Component {
+class BigCards extends Component {
+  handleClick() {
+    alert('clicked!');
+  };
 
   render() {
     return (
       <div>
         <img src={`http://lorempixel.com/400/200/sports/${this.props.image}`} />
-        <p>{this.props.title}</p>
+        <a href={this.props.url} onClick={this.handleClick}><p>{this.props.title}</p></a>
       </div>
     );
   }
 }
 
 class MidCards extends Component {
+  handleClick() {
+    alert('clicked!');
+  };
 
   render() {
     return (
       <div>
         <img src={`http://lorempixel.com/200/100/food/${this.props.image}`} />
-        <p>{this.props.title}</p>
+        <a href={this.props.url} onClick={this.handleClick}><p>{this.props.title}</p></a>
       </div>
     );
   }
 }
 
 class SmallCards extends Component {
+  handleClick() {
+    alert('clicked!');
+  };
 
   render() {
     return (
       <div>
-        <p>{this.props.title}</p>
+        <a href={this.props.url} onClick={this.handleClick}><p>{this.props.title}</p></a>
       </div>
     );
   }
